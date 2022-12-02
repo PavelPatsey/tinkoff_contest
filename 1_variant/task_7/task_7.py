2a9b9e7fbb2c0cd51f611115030cbb019c3c6da5
Павел Пацей <patsey.pavel@gmail.com>
2022-12-02 15:33:28 +0300

    writing task_7

--- 1_variant/task_7/task_7.py
+++ 1_variant/task_7/task_7.py
@@ -4,23 +4,26 @@ from itertools import permutations
 
 def is_circular(students_len, students):
     stack = []
-    next_student_index = 0
-    stack.append(next_student_index)
-    for _ in range(students_len):
-        current_student_index = next_student_index
-        next_student_index = students[current_student_index] - 1
-        stack.append(next_student_index)
-
+    next_student_number = 1
+    i = 0
+    while i < students_len:
+        current_student_number = next_student_number
+        next_student_number = students[current_student_number - 1]
+        stack.append(next_student_number)
+        i += 1
+        
     counter = Counter(stack)
-    print(counter)
-    print(stack)
     return len(counter) == students_len
 
 
 def redirect_gift(students, from_whom_number, to_whom_number):
     new_students = students.copy()
     new_students[from_whom_number - 1] = to_whom_number
-    print(f"{new_students=}")
+    
+
+    isCircular = is_circular(len(students), new_students)
+    print(f"{new_students=} , {isCircular=}")
+
     return new_students
 
 
@@ -55,20 +58,23 @@ def read_input():
 def main():
     # students_len, students = read_input()
     # print(*get_answer(students_len, students))
-    # print(is_circular(3, [2, 3, 2]))
+
+    # print(get_answer(2, [2, 2]))
+    print(get_answer(3, [2, 3, 1]))
     pass
 
 
 if __name__ == "__main__":
-    assert is_circular(3, [1, 3, 1]) == False
+    # assert is_circular(3, [1, 3, 1]) == False
     # assert is_circular(3, [2, 3, 1]) == True
     # assert is_circular(5, [2, 1, 4, 5, 3]) == False
-    # assert is_circular(3, [2, 3, 2]) == False
+    assert is_circular(3, [2, 3, 2]) == False
 
     # assert get_answer(3, [1, 2, 3]) == (-1, -1)
     # assert get_answer(3, [1, 3, 1]) == (1, 2)
     # assert get_answer(5, [2, 1, 2, 3, 4]) == (1, 5)
-    # assert get_answer(5, [2, 1, 4, 5, 3]) == (2, 3)
+    # assert get_answer(5, [2, 1, 4, 5, 3]) == (-1, -1)
+    # assert get_answer(2, [2, 2]) == (2, 1)
     main()
 
 
@@ -95,7 +101,7 @@ if __name__ == "__main__":
 5
 2 1 4 5 3
 Вывод
-2 3
+-1 -1
 
 Ввод
 3
